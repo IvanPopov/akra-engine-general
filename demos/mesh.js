fbce56d0cabc91be5aa83daee024b83b4941b676
IvanPopov <vantuziast@odserve.org>
2012-06-29 18:27:10 +0300

    Examples modified.

--- demos/mesh.js
+++ demos/mesh.js
@@ -1,3 +1,87 @@
+function buildCubeMesh (pEngine, eOptions, sName) {
+	sName = sName || 'cube';
+
+    var pMesh,
+        pSubMesh;
+    var iPos, iNorm;
+
+    var pVerticesData = new Float32Array([
+        -0.5, 0.5, 0.5,
+        0.5, 0.5, 0.5,
+        -0.5, -0.5, 0.5,
+        0.5, -0.5, 0.5,
+        -0.5, 0.5, -0.5,
+        0.5, 0.5, -0.5,
+        -0.5, -0.5, -0.5,
+        0.5, -0.5, -0.5
+    ]);
+
+    var pNormalsData = new Float32Array([
+        1.0, 0.0, 0.0,
+        -1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, -1.0, 0.0,
+        0.0, 0.0, 1.0,
+        0.0, 0.0, -1.0
+    ]);
+
+    var pVertexIndicesData = new Float32Array([
+        0, 2, 3, 0, 3, 1,
+        0, 1, 5, 0, 5, 4,
+        6, 7, 3, 6, 3, 2,
+        0, 4, 6, 0, 6, 2,
+        3, 7, 5, 3, 5, 1,
+        5, 7, 6, 5, 6, 4
+    ]);
+    var pNormalIndicesData = new Float32Array([
+        4, 4, 4, 4, 4, 4,
+        2, 2, 2, 2, 2, 2,
+        3, 3, 3, 3, 3, 3,
+        1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0,
+        5, 5, 5, 5, 5, 5
+    ]);
+
+    var iBegin = a.now(), iTime = 0;
+    pMesh = new a.Mesh(pEngine, eOptions, sName);
+ 
+    trace(++iTime, 'time: ', a.now() - iBegin, 'ms');
+    //iPos = pMesh.setData([VE_VEC3('POSITION')], pVerticesData);
+    //iNorm = pMesh.setData([VE_VEC3('NORMAL')], pNormalsData);
+    //
+    //iIPos = pMesh.setData([VE_FLOAT('INDEX1')], pVertexIndicesData);
+    //iINorm = pMesh.setData([VE_FLOAT('INDEX2')], pNormalIndicesData);
+    
+
+    pSubMesh = pMesh.allocateSubset();
+
+    trace(++iTime, 'time: ', a.now() - iBegin, 'ms');
+    var iNorm = pSubMesh.allocateData([VE_VEC3('NORMAL')], pNormalsData);
+    
+    trace(++iTime, 'time: ', a.now() - iBegin, 'ms');
+    var iPos = pSubMesh.allocateData([VE_VEC3('POSITION')], pVerticesData);
+    
+    trace(++iTime, 'time: ', a.now() - iBegin, 'ms');
+    pMesh.addMaterial('default');
+   
+	
+	trace(++iTime, 'time: ', a.now() - iBegin, 'ms');
+    
+    pSubMesh.allocateIndex([VE_FLOAT('INDEX1')], pVertexIndicesData);
+    pSubMesh.allocateIndex([VE_FLOAT('INDEX2')], pNormalIndicesData);
+       
+    pSubMesh.setMaterial('default');
+
+    pSubMesh.index(iPos, 'INDEX1');
+    pSubMesh.index(iNorm, 'INDEX2');
+	trace('mesh creation time: ', a.now() - iBegin, 'ms');
+    trace(pSubMesh._pMap.toString());
+
+    return pMesh;
+}
+
+
+
 function MyGame() {
 	A_CLASS;
 };
@@ -25,6 +109,7 @@ MyGame.prototype.initDeviceObjects = function () {
 	this.notifyInitDeviceObjects();
 	
 	trace(buildCubeMesh(this, 0));
+	trace(buildCubeMesh(this, 0, 'cube2'));
 
 	return true;
 };
