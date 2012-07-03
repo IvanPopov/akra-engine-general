b80e53c9a977d314bb21430581e53c757b9b558b
Sergey Semenov <sss@odserve.org>
2012-07-03 19:54:49 +0300

    Tests are started

---
+++ demos/parser.js
@@ -0,0 +1,18 @@
+Include("../../akra-engine-core/EffectParser/parser.js");
+Include("../../akra-engine-core/EffectParser/Effect.js");
+
+var pParser = new Parser();
+var sGrammar, sSource;
+a.fopen("/akra-engine-general/media/grammars/HLSL_grammar.gr", "r").read(function (pData) {
+	sGrammar = pData;
+	a.fopen("/akra-engine-general/media/effects/Demo.fx", "r").read(function (pData1) {
+		sSource = pData1;
+		pParser.init(sGrammar);
+		pParser.parse(sSource);
+        var pTree= pParser.pSyntaxTree;
+        trace(pTree);
+        var pEffect = new Effect();
+        pEffect.analyze(pTree);
+        trace(pEffect);
+	});
+});
