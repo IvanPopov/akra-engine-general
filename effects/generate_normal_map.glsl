5ae8eed91e5a52c44e106a63536110a8226035ac
reinor <iakarateev@gmail.com>
2012-08-01 14:23:28 +0400

    закончена работа над параллаксом

--- effects/generate_normal_map.glsl
+++ effects/generate_normal_map.glsl
@@ -2,7 +2,7 @@ attribute vec2 POSITION;
 varying vec2 texturePosition;
 
 void main(void){
-    texturePosition = (POSITION + vec2(1.,1.))/2.;
+    texturePosition = (POSITION + vec2(1.))/2.;
     gl_Position = vec4(POSITION,0.,1.);
 }
 
