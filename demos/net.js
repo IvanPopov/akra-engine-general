e6faa3edf7c980a46ba974ba91ba59ceabe85176
IvanPopov <vantuziast@odserve.org>
2012-08-17 18:17:08 +0300

    Добавлено демо, демонстрирующее возможности RPC.

---
+++ demos/net.js
@@ -0,0 +1,27 @@
+function onConnection(pErr) {
+	debug_assert(pErr, pErr.message);
+
+	trace('connected.');
+}
+
+
+
+var pPipe = a.NET.pipe('ws://localhost');
+
+if (pPipe) {
+	pPipe.on('open', function () {
+		pPipe.rpc();
+
+		var n = 0;
+
+		for (var i = 1000; i --;) {
+			(function (id) { 
+				pPipe.proc('echo', id, function (pRes) {
+					trace(id, '==', pRes, 'test: ', pRes === id? 'passed': 'failed');
+				});
+			})(Math.floor(Math.random() * 10000));
+		}
+
+	})
+}
+
