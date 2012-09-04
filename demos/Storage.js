0670e4093a5fbdfa9b85e26bf11cd03d2b8098d3
Sergey Semenov <sss@odserve.org>
2012-09-04 20:40:47 +0400

    little commi

--- demos/Storage.js
+++ demos/Storage.js
@@ -4,10 +4,11 @@ function PopArray(nLength, iIncrement, eMode) {
              ADD = 2
          ], ARRAYWRITEMODE, a.SystemArray);
 
-    this._pData = nLength !== undefined ? new Array(nLength) : null;
+    this._pData = new Array(nLength || 100);
     this._nCount = 0;
     this._eMode = eMode || a.SystemArray.ADD;
-    this._iIncrement = iIncrement || 0;
+    this._iIncrement = iIncrement || 10;
+    this._pAllocator = null;
 }
 A_NAMESPACE(PopArray);
 
@@ -15,15 +16,6 @@ PROPERTY(PopArray, "length",
          function () {
              return this._nCount
          });
-
-PopArray.prototype.create = function (nLength, iIncrement, eMode) {
-    nLength = nLength || 100;
-    iIncrement = iIncrement || 10;
-    eMode = eMode || a.SystemArray.ADD;
-    this._pData = new Array(nLength);
-    this._eMode = eMode;
-    this._nCount = 0;
-};
 
 PopArray.prototype.release = function (isStrong) {
     this._nCount = 0;
@@ -33,6 +25,9 @@ PopArray.prototype.release = function (isStrong) {
         for (var i = 0; i < iLength; i++) {
             pData[i] = null;
         }
+    }
+    if (this._pAllocator !== null) {
+        this._pAllocator._releaseElement(this);
     }
 };
 
@@ -66,30 +61,28 @@ PopArray.prototype.setElement = function (index, pValue) {
     this._pData[index] = pValue;
 };
 
-PopArray.prototype.element = function(index){
+PopArray.prototype.element = function (index) {
     if (index === undefined || index >= this._nCount) {
         return null;
     }
     return this._pData[index];
 };
 
+
 function Map() {
     this.pKeys = new a.PopArray();
     this.pValues = new a.PopArray();
-    this._pMap = null;
+    this._pMap = {};
+    this._pAllocator = null;
 }
 A_NAMESPACE(Map);
-
-
-Map.prototype.create = function () {
-    this.pKeys.create();
-    this.pValues.create();
-    this._pMap = {};
-};
 
 Map.prototype.release = function (isStrong) {
     this.pKeys.release(isStrong);
     this.pValues.release(isStrong);
+    if (this._pAllocator !== null) {
+        this._pAllocator._releaseElement(this);
+    }
 };
 
 Map.prototype.addElement = function (sKey, pValue) {
@@ -106,7 +99,7 @@ Map.prototype.addElement = function (sKey, pValue) {
         pMap[sKey] = iSize;
     }
 //    else if (sOldKey === sKey) {
-        pValues.setElement(index, pValue);
+    pValues.setElement(index, pValue);
 //    }
 };
 
@@ -115,48 +108,178 @@ Map.prototype.hasElement = function (sKey) {
     return (index !== undefined && this.pKeys.element(index) === sKey);
 };
 
-Map.prototype.element = function(sKey) {
+Map.prototype.element = function (sKey) {
     var index = this._pMap[sKey];
     return (index !== undefined && this.pKeys.element(index) === sKey) ? this.pValues[index] : null;
 };
 
+//function MapAllocator(nCount, iIncrement) {
+//    this._pElements = new Array(nCount || 100);
+//    this._nCount = 0;
+//    this._iIncrement = iIncrement || 10;
+//    for (var i = 0; i < this._pElements.length; i++) {
+//        this._pElements[i] = new a.Map();
+//        this._pElements[i].index = i;
+//    }
+//}
 
-function test1(){
-    var obj = {};
-    for(var i = 0; i < 1000; i++){
-        obj["Prop"+i] = i*i-1;
+function Allocator(fnConstructor, nCount, iIncrement) {
+    if (!fnConstructor) {
+        return false;
     }
-    return obj;
+    this._pElements = new Array(nCount || 100);
+    this._nCount = 0;
+    this._iIncrement = iIncrement || 10;
+    this._fnConstructor = fnConstructor;
+
+    for (var i = 0; i < this._pElements.length; i++) {
+        this._pElements[i] = new this._fnConstructor();
+        this._pElements[i]._pAllocator = this;
+    }
 }
 
-function test2(myObj){
-    myObj.release();
-    for(var i = 0; i < 1000; i++){
-        myObj.addElement("Prop"+i, i*i-1);
+Allocator.prototype.getElement = function () {
+    var pElements = this._pElements;
+    if (pElements.length === this._nCount) {
+        this._addElements();
     }
-    return myObj;
+    return pElements[this._nCount++];
+};
+
+Allocator.prototype._addElements = function () {
+    var pElements = this._pElements;
+    var pElement;
+    for (var i = 0; i < this._iIncrement; i++) {
+        pElement = new this._fnConstructor();
+        pElements.push(pElement);
+        pElement._pAllocator = this;
+    }
+};
+
+Allocator.prototype._releaseElement = function (pElement) {
+    this._pElements[--this._nCount] = pElement;
+};
+
+a._pMapAllocator = new Allocator(a.Map, 1000, 100);
+a._pArrayAllocator = new Allocator(a.PopArray, 1000, 100);
+
+function PopArrayStorage() {
+    return a._pArrayAllocator.getElement();
 }
 
-function timeWrapper(pFunc, nTimes, arg1){
+function MapStorage() {
+    return a._pMapAllocator.getElement();
+}
+
+var pObj, pObj1, pObj2, pObj3, pObj4;
+var pMap, pMap1, pMap2, pMap3, pMap4;
+function test1() {
+    pObj = new Object();
+//    pObj1 = new Object();
+//    pObj2 = new Object();
+//    pObj3 = new Object();
+//    pObj4 = new Object();
+    for (var i = 0; i < 1000; i++) {
+        pObj["Prop_" + i] = i * i + 1;
+//        pObj1["Prope_" + i] = i * i + 1;
+//        pObj2["Proper_" + i] = i * i + 1;
+//        pObj3["Propert_" + i] = i * i + 1;
+//        pObj4["Property_" + i] = i * i + 1;
+    }
+//    var pKeys = Object.keys(pObj);
+//    var sKey;
+//    for(var i = 0; i < pKeys.length; i++){
+//        sKey = pKeys[i];
+//        pObj[sKey] = pObj[sKey]*(pObj[sKey]-1);
+//    }
+
+    for (var i in pObj) {
+        pObj[i] = pObj[i] * (pObj[i] - 1);
+    }
+}
+function test2() {
+    pMap = new MapStorage();
+//    pMap1 = new MapStorage();
+//    pMap2 = new MapStorage();
+//    pMap3 = new MapStorage();
+//    pMap4 = new MapStorage();
+    for (var i = 0; i < 1000; i++) {
+        pMap.addElement("Prop_" + i, i * i + 1);
+//        pMap1.addElement("Prope_" + i, i * i + 1);
+//        pMap2.addElement("Proper_" + i, i * i + 1);
+//        pMap3.addElement("Propert_" + i, i * i + 1);
+//        pMap4.addElement("Property_" + i, i * i + 1);
+    }
+    var pValues = pMap.pValues;
+    var iLength = pMap.pValues.length;
+    for (var i = 0; i < iLength; i++) {
+        pValues._pData[i] = pValues._pData[i] * (pValues._pData[i] - 1);
+    }
+}
+
+function timeWrapper(pFunc, nTimes, arg1) {
     var i, time = new Date();
-    for(i = 0; i < nTimes; i++){
-        pFunc.call(null, arg1);
-    }
+    eval("for (var i = 0; i < nTimes; i++) { pFunc.call(null, arg1);}");
     return new Date() - time;
 }
 
+trace("1: ", timeWrapper(test1, 100));
+trace("2: ", timeWrapper(test2, 100));
 
-var myObj = new Map();
-myObj.create();
+trace(a._pMapAllocator);
 
-trace("Base: ", timeWrapper(test1, 1000));
-trace("My: ", timeWrapper(test2, 1000, myObj));
+trace("1: ", timeWrapper(test1, 100));
+trace("2: ", timeWrapper(test2, 100));
 
-trace(myObj);
 
-trace("Base: ", timeWrapper(test1, 1000));
-trace("My: ", timeWrapper(test2, 1000, myObj));
+//
+//function mapTest1() {
+//    var obj = {};
+//    for (var i = 0; i < 1000; i++) {
+//        obj["Prop" + i] = i * i - 1;
+//    }
+//    return obj;
+//}
+//
+//function mapTest2(myObj) {
+//    myObj.release();
+//    for (var i = 0; i < 1000; i++) {
+//        myObj.addElement("Prop" + i, i * i - 1);
+//    }
+//    return myObj;
+//}
+//var myObj = new Map();
+//myObj.create();
+//
+//trace("Base: ", timeWrapper(mapTest1, 1000));
+//trace("My: ", timeWrapper(mapTest2, 1000, myObj));
+//
+//trace(myObj);
+//
+//trace("Base: ", timeWrapper(mapTest1, 1000));
+//trace("My: ", timeWrapper(mapTest2, 1000, myObj));
+//
+//trace("-------------");
 
-trace("-------------");
+
+//function test1(arr) {
+//    for (var i = 0; i < 100; i++) {
+//        arr.push({});
+//    }
+//}
+//
+//function test2(arr) {
+//    arr += 100;
+//    for (var i = arr.length - 100; i < arr.length; i++) {
+//        arr[i] = {};
+//    }
+//}
+//
+//trace("1: ", timeWrapper(test1, 1000, []));
+//trace("2: ", timeWrapper(test2, 1000, []));
+//
+//
+//trace("1: ", timeWrapper(test1, 1000, []));
+//trace("2: ", timeWrapper(test2, 1000, []));
 
 
--- demos/parser.js
+++ demos/parser.js
@@ -15,6 +15,7 @@ EXTENDS(ShaderDemo, a.Engine);
 ShaderDemo.prototype.oneTimeSceneInit = function () {
     'use strict';
     this.notifyOneTimeSceneInit();
+    this.setupWorldOcTree(new a.Rect3d(-500.0, 500.0, -500.0, 500.0, 0.0, 500.0));
     this.showStats(true);
 //    A_TRACER.BEGIN();
     this.pTexture0 = this.displayManager().texturePool().loadResource("/akra-engine-general/media/textures/lion.png");
@@ -44,16 +45,18 @@ ShaderDemo.prototype.initDeviceObjects = function () {
     time = new Date();
     function addMeshToScene(pEngine, pMesh, pParent) {
         var pSceneObject = new a.SceneModel(pEngine, pMesh);
-//        pSceneObject.create();
-//        pSceneObject.attachToParent(pParent || pEngine.getRootNode());
+        pSceneObject.create();
+        pSceneObject.attachToParent(pParent || pEngine.getRootNode());
         return pSceneObject;
     }
 
     this.pPlane = addMeshToScene(this, sceneSurface(this));
     this.pPlane.bNoRender = true;
+    this.pPlane.setScale(200.0);
     pEffectResource = this.pPlane._pMeshes[0][0]._pActiveSnapshot._pRenderMethod._pEffect;
     pEffectResource.create();
     pEffectResource.use(this.shaderManager().getComponentByName("akra.system.plane"));
+
     this.pCube = new Array(1);
     for (var i = 0; i < this.pCube.length; i++) {
         this.pCube[i] = addMeshToScene(this, cube(this));
@@ -72,58 +75,6 @@ ShaderDemo.prototype.initDeviceObjects = function () {
         pSurface.setTexture(1, this.pTexture0, 2);
     }
 
-    var pSnapshot;
-    var pMap;
-    var pEntry = [];
-    //PLANE
-    window['A_TRACER.trace']('before PLANE');
-    pSnapshot = this.pPlane._pMeshes[0][0]._pActiveSnapshot;
-    pMap = this.pPlane._pMeshes[0][0]._pRenderData._pMap;
-    pSnapshot.begin();
-    pSnapshot.activatePass(0);
-    pManager.setViewport(0, 0, this.pCanvas.width, this.pCanvas.height);
-    pSnapshot.setParameter("model_mat", [
-        200, 0, 0, 0,
-        0, 200, 0, 0,
-        0, 0, 200, 0,
-        0, 0, 0, 1]);
-    pSnapshot.applyBufferMap(pMap);
-    pEntry.push(pSnapshot.renderPass());
-    pSnapshot.deactivatePass();
-    pSnapshot.end();
-//    pManager.render(pEntry1);
-    //CUBE
-    window['A_TRACER.trace']('before CUBE');
-    for (var i = 0; i < this.pCube.length; i++) {
-        pSnapshot = this.pCube[i]._pMeshes[0][0]._pActiveSnapshot;
-//    trace("CUBE surface:", pSnapshot.surfaceMaterial);
-        pMap = this.pCube[i]._pMeshes[0][0]._pRenderData._pMap;
-        pSnapshot.begin();
-        pSnapshot.activatePass(0);
-        pManager.setViewport(0, 0, this.pCanvas.width, this.pCanvas.height);
-        pSnapshot.setParameter("model_mat", [
-            1, 0, 0, 0,
-            0, 1, 0, 0,
-            0, 0, 1, 0,
-            0, 0, 0, 1]);
-        pSnapshot.setParameter("normal_mat", [
-            1, 0, 0,
-            0, 1, 0,
-            0, 0, 1]);
-//    pSnapshot.setParameter("test_uniform", {a : 1.0, b : {x : 0.5, y : 0.3}});
-//    pSnapshot.setParameter("test_uniform.b.x", 0.1);
-//    pSnapshot.setParameter("UTEST0", {TA : 0.7, TB : {TX : 0.7, TY : 0.8}}, true);
-//    pSnapshot.setParameterBySemantic("UTEST0.TB.TX", 0.4);
-        pSnapshot.applyBufferMap(pMap);
-        pSnapshot.applySurfaceMaterial();
-        pEntry.push(pSnapshot.renderPass());
-        pSnapshot.deactivatePass();
-        pSnapshot.end();
-    }
-//    pManager.render(pEntry2);
-    this.pEntry = pEntry;
-
-    window['A_TRACER.trace']('before RENDER');
 //    trace("PLANE: ", pEntry1);
 //    trace("CUBE: ", pEntry2);
 //    for(var i = 0; i < pEntry.length; i++){
@@ -138,74 +89,74 @@ ShaderDemo.prototype.initDeviceObjects = function () {
 
 ShaderDemo.prototype.directRender = function () {
     'use strict';
-    var pManager = this.shaderManager();
-    /*
-     //    A_TRACER.BEGIN();
-     var pManager = this.shaderManager();
-     var pSnapshot;
-     var pMap;
-     var pEntry = [];
-     //PLANE
-     window['A_TRACER.trace']('before PLANE');
-     pSnapshot = this.pPlane._pMeshes[0][0]._pActiveSnapshot;
-     pMap = this.pPlane._pMeshes[0][0]._pRenderData._pMap;
-     pSnapshot.begin();
-     pSnapshot.activatePass(0);
-     pManager.setViewport(0, 0, this.pCanvas.width, this.pCanvas.height);
-     pSnapshot.setParameter("model_mat", [
-     200, 0, 0, 0,
-     0, 200, 0, 0,
-     0, 0, 200, 0,
-     0, 0, 0, 1]);
-     pSnapshot.applyBufferMap(pMap);
-     pEntry.push(pSnapshot.renderPass());
-     pSnapshot.deactivatePass();
-     pSnapshot.end();
-     //    pManager.render(pEntry1);
-     //CUBE
-     window['A_TRACER.trace']('before CUBE');
-     for (var i = 0; i < this.pCube.length; i++) {
-     pSnapshot = this.pCube[i]._pMeshes[0][0]._pActiveSnapshot;
-     //    trace("CUBE surface:", pSnapshot.surfaceMaterial);
-     pMap = this.pCube[i]._pMeshes[0][0]._pRenderData._pMap;
-     pSnapshot.begin();
-     pSnapshot.activatePass(0);
-     pManager.setViewport(0, 0, this.pCanvas.width, this.pCanvas.height);
-     pSnapshot.setParameter("model_mat", [
-     1, 0, 0, 0,
-     0, 1, 0, 0,
-     0, 0, 1, 0,
-     0, 0, 0, 1]);
-     pSnapshot.setParameter("normal_mat", [
-     1, 0, 0,
-     0, 1, 0,
-     0, 0, 1]);
-     //    pSnapshot.setParameter("test_uniform", {a : 1.0, b : {x : 0.5, y : 0.3}});
-     //    pSnapshot.setParameter("test_uniform.b.x", 0.1);
-     //    pSnapshot.setParameter("UTEST0", {TA : 0.7, TB : {TX : 0.7, TY : 0.8}}, true);
-     //    pSnapshot.setParameterBySemantic("UTEST0.TB.TX", 0.4);
-     pSnapshot.applyBufferMap(pMap);
-     pSnapshot.applySurfaceMaterial();
-     pEntry.push(pSnapshot.renderPass());
-     pSnapshot.deactivatePass();
-     pSnapshot.end();
-     }
-     //    pManager.render(pEntry2);
-
-     window['A_TRACER.trace']('before RENDER');
-     //    trace("PLANE: ", pEntry1);
-     //    trace("CUBE: ", pEntry2);
-     for(var i = 0; i < pEntry.length; i++){
-     pManager.render(pEntry[i]);
-     }
-     //    pManager.render(pEntry1);
-     //    pManager.render(pEntry2);
-
-     //    this.pause(true);
-     //    A_TRACER.END();*/
-    for (var i = 0; i < this.pEntry.length; i++) {
-        pManager.render(this.pEntry[i]);
-    }
+//    var pManager = this.shaderManager();
+//
+//     //    A_TRACER.BEGIN();
+//     var pManager = this.shaderManager();
+//     var pSnapshot;
+//     var pMap;
+//     var pEntry = [];
+//     //PLANE
+//     window['A_TRACER.trace']('before PLANE');
+//     pSnapshot = this.pPlane._pMeshes[0][0]._pActiveSnapshot;
+//     pMap = this.pPlane._pMeshes[0][0]._pRenderData._pMap;
+//     pSnapshot.begin();
+//     pSnapshot.activatePass(0);
+//     pManager.setViewport(0, 0, this.pCanvas.width, this.pCanvas.height);
+//     pSnapshot.setParameter("model_mat", [
+//     200, 0, 0, 0,
+//     0, 200, 0, 0,
+//     0, 0, 200, 0,
+//     0, 0, 0, 1]);
+//     pSnapshot.applyBufferMap(pMap);
+//     pEntry.push(pSnapshot.renderPass());
+//     pSnapshot.deactivatePass();
+//     pSnapshot.end();
+//     //    pManager.render(pEntry1);
+//     //CUBE
+//     window['A_TRACER.trace']('before CUBE');
+//     for (var i = 0; i < this.pCube.length; i++) {
+//     pSnapshot = this.pCube[i]._pMeshes[0][0]._pActiveSnapshot;
+//     //    trace("CUBE surface:", pSnapshot.surfaceMaterial);
+//     pMap = this.pCube[i]._pMeshes[0][0]._pRenderData._pMap;
+//     pSnapshot.begin();
+//     pSnapshot.activatePass(0);
+//     pManager.setViewport(0, 0, this.pCanvas.width, this.pCanvas.height);
+//     pSnapshot.setParameter("model_mat", [
+//     1, 0, 0, 0,
+//     0, 1, 0, 0,
+//     0, 0, 1, 0,
+//     0, 0, 0, 1]);
+//     pSnapshot.setParameter("normal_mat", [
+//     1, 0, 0,
+//     0, 1, 0,
+//     0, 0, 1]);
+//     //    pSnapshot.setParameter("test_uniform", {a : 1.0, b : {x : 0.5, y : 0.3}});
+//     //    pSnapshot.setParameter("test_uniform.b.x", 0.1);
+//     //    pSnapshot.setParameter("UTEST0", {TA : 0.7, TB : {TX : 0.7, TY : 0.8}}, true);
+//     //    pSnapshot.setParameterBySemantic("UTEST0.TB.TX", 0.4);
+//     pSnapshot.applyBufferMap(pMap);
+//     pSnapshot.applySurfaceMaterial();
+//     pEntry.push(pSnapshot.renderPass());
+//     pSnapshot.deactivatePass();
+//     pSnapshot.end();
+//     }
+//     //    pManager.render(pEntry2);
+//
+//     window['A_TRACER.trace']('before RENDER');
+//     //    trace("PLANE: ", pEntry1);
+//     //    trace("CUBE: ", pEntry2);
+//     for(var i = 0; i < pEntry.length; i++){
+//        pManager.render(pEntry[i]);
+//     }
+//     //    pManager.render(pEntry1);
+//     //    pManager.render(pEntry2);
+//
+//     //    this.pause(true);
+//     //    A_TRACER.END();*/
+////    for (var i = 0; i < this.pEntry.length; i++) {
+////        pManager.render(this.pEntry[i]);
+////    }
 };
 
 ShaderDemo.prototype.deleteDeviceObjects = function () {
